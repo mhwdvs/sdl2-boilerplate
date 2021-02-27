cf9d771aaf22ef5a6c9f091191748ede9fef2863
Matthew Davis <matt@mhwdvs.com>
2021-02-27 19:23:57 +0800

    Access return value of RUN_UNIT_TESTS()

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,13 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
+    for(size_t i = 0; i < size_t (argc); ++i){
+        if(strcmp(argv[i], "--test") == 0){ // only run tests if requested
+            ::testing::InitGoogleTest(&argc, argv);
+            auto res = RUN_ALL_TESTS();
+            std::cout << "Unit tests returned: " << res << '\n';
+        }
+    }
     std::cout << "Hello World!\n";
 	return 0;
 }
