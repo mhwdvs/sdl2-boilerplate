9edfb099a924be51b2870bbed794a0ca426021f0
Matthew Davis <matt@mhwdvs.com>
2021-02-28 13:14:33 +0800

    Add google test, currently runs regardless of build type

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,8 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
-    for(size_t i = 0; i < size_t (argc); ++i){
-        if(strcmp(argv[i], "--test") == 0){ // only run tests if requested
-            ::testing::InitGoogleTest(&argc, argv);
-            auto res = RUN_ALL_TESTS();
-            std::cout << "Unit tests returned: " << res << '\n';
-        }
-    }
+    ::testing::InitGoogleTest(&argc, argv);
     std::cout << "Hello World!\n";
+    EXPECT_TRUE(argc == 0) << "Tests are working!\n";
 	return 0;
 }
