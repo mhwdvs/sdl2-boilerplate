6b2d1011f1fc86f670111ce066ec821486d4b14e
Matthew Davis <matt@mhwdvs.com>
2021-03-01 14:48:55 +0800

    yeah im lost

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
     std::cout << "Hello World!\n";
-    EXPECT_TRUE(argc == 0) << "Tests are working!\n";
 	return 0;
 }
+
+int Factorial(int n){
+    return 0;
+}
+
