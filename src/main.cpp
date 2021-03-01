db53dd48049a2690381f0785d548aa6f558136fa
Matthew Davis <matt@mhwdvs.com>
2021-03-01 21:22:56 +0800

    ITS ALIVEEEE

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,3 @@ int main(int argc, char **argv) {
     std::cout << "Hello World!\n";
 	return 0;
 }
-
-int Factorial(int n){
-    return 0;
-}
-
--- test/testlib.cpp
+++ test/testlib.cpp
@@ -1,22 +1,6 @@
-#include <iostream>
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    std::cout << "Hello World!\n";
-    RUN_ALL_TESTS();
-	return 0;
-}
-
-// Tests factorial of 0.
-TEST(FactorialTest, HandlesZeroInput) {
-    EXPECT_EQ(Factorial(0), 1);
-}
-
-// Tests factorial of positive numbers.
-TEST(FactorialTest, HandlesPositiveInput) {
-    EXPECT_EQ(Factorial(1), 1);
-    EXPECT_EQ(Factorial(2), 2);
-    EXPECT_EQ(Factorial(3), 6);
-    EXPECT_EQ(Factorial(8), 40320);
+    return RUN_ALL_TESTS();
 }
