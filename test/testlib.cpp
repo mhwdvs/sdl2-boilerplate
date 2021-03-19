3ff002e599822da5b84633565226cc27627a63ab
Matthew Davis <matt@mhwdvs.com>
2021-03-19 20:48:35 +0800

    Reimplement GoogleTest

--- test/testlib.cpp
+++ test/testlib.cpp
@@ -1,7 +1,7 @@
-//#include "gtest/gtest.h"
+#include "gtest/gtest.h"
 #include <Car.h>
 
 int main(int argc, char **argv) {
-    //::testing::InitGoogleTest(&argc, argv);
-    //return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
