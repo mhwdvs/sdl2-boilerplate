13534c2fb3448c8517b0d158ffcc80df479485d4
Matthew Davis <matt@mhwdvs.com>
2021-06-22 00:34:57 +0800

    Add SDL submodule and sample SDL code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,62 @@
 #include <iostream>
+#include <cstdlib>
+#include <SDL.h>
 
 int main() {
-    std::cout << "Hello World!\n";
-	return 0;
+    using std::cerr;
+    using std::endl;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        cerr << "SDL_Init Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        cerr << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer* ren
+            = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr) {
+        cerr << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Surface* bmp = SDL_LoadBMP("img/grumpy-cat.bmp");
+    if (bmp == nullptr) {
+        cerr << "SDL_LoadBMP Error: " << SDL_GetError() << endl;
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+    if (tex == nullptr) {
+        cerr << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
+        SDL_FreeSurface(bmp);
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    SDL_FreeSurface(bmp);
+
+    for (int i = 0; i < 20; i++) {
+        SDL_RenderClear(ren);
+        SDL_RenderCopy(ren, tex, nullptr, nullptr);
+        SDL_RenderPresent(ren);
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
