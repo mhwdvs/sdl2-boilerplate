9f7cb33ebd14147b42af0b5569f16e0149612962
Matthew Davis <matt@mhwdvs.com>
2021-06-22 17:48:55 +0800

    Fix actions

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <SDL.h>
 
-int main() {
+int main(int argc, char** argv) {
     using std::cerr;
     using std::endl;
 
@@ -17,13 +17,22 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    SDL_Renderer* ren
-            = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (ren == nullptr) {
-        cerr << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
-        SDL_DestroyWindow(win);
-        SDL_Quit();
-        return EXIT_FAILURE;
+    bool shouldRender = true;
+    if(argc == 2 && std::string(argv[1]) == "--headless"){
+        // dont create or destory any renderers
+        shouldRender = false;
+    }
+
+    SDL_Renderer* ren = nullptr;
+    if(shouldRender){
+        ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+        if (ren == nullptr) {
+            cerr << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
+            SDL_DestroyWindow(win);
+            SDL_Quit();
+            return EXIT_FAILURE;
+        }
     }
 
     SDL_Surface* bmp = SDL_LoadBMP("img/grumpy-cat.bmp");
@@ -35,26 +44,30 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
-    if (tex == nullptr) {
-        cerr << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
+    if(shouldRender){
+        SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+        if (tex == nullptr) {
+            cerr << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
+            SDL_FreeSurface(bmp);
+            SDL_DestroyRenderer(ren);
+            SDL_DestroyWindow(win);
+            SDL_Quit();
+            return EXIT_FAILURE;
+        }
+
         SDL_FreeSurface(bmp);
+
+        for (int i = 0; i < 20; i++) {
+            SDL_RenderClear(ren);
+            SDL_RenderCopy(ren, tex, nullptr, nullptr);
+            SDL_RenderPresent(ren);
+            SDL_Delay(100);
+        }
+
+        SDL_DestroyTexture(tex);
         SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        SDL_Quit();
-        return EXIT_FAILURE;
-    }
-    SDL_FreeSurface(bmp);
 
-    for (int i = 0; i < 20; i++) {
-        SDL_RenderClear(ren);
-        SDL_RenderCopy(ren, tex, nullptr, nullptr);
-        SDL_RenderPresent(ren);
-        SDL_Delay(100);
     }
-
-    SDL_DestroyTexture(tex);
-    SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
 
